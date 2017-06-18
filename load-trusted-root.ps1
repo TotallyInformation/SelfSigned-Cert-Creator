09bd2df23a265f49cba73c8fb2cfc51995222520
Julian Knight <jk.github@totallyinformation.com>
2017-06-18 21:47:42 +0100

    Update load-trusted-root.ps1

--- load-trusted-root.ps1
+++ load-trusted-root.ps1
@@ -1,5 +1,6 @@
 #import cert chain
+# @see https://technet.microsoft.com/en-us/itpro/powershell/windows/pkiclient/import-certificate
 # @see last answer at https://social.technet.microsoft.com/Forums/en-US/8e016573-9191-4152-8c4e-b74d739f5894/powershell-to-add-a-certificate-to-trusted-root-certification-authorities?forum=winserverpowershell
 
-$p7b = '\\dc2012\CertEnroll\FoxDeployCAChain.p7b'
-Import-Certificate -FilePath $p7b -CertStoreLocation Cert:\LocalMachine\Root
+$myTrustedCertChain = '.\certs\client\my-private-root-ca.crt'
+Import-Certificate -FilePath $myTrustedCertChain -CertStoreLocation Cert:\LocalMachine\Root
